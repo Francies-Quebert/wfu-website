c2db11fc7dda1121fddd4a7e507eae5ac681f06c
francies <quebert88@gmail.com>
2024-04-21 18:59:22 +0100

    updating env file name

--- utility/actions.ts
+++ utility/actions.ts
@@ -6,8 +6,8 @@ import { randomUUID } from 'crypto';
 const s3Client = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    accessKeyId: process.env.UPLOAD_ACCESS_KEY!,
+    secretAccessKey: process.env.UPLOAD_SECRET_KEY!,
   },
 });
 
